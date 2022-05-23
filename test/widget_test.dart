12b2046b014b4008751ba36dcaba9648a4294958
Bruno Alves(furious) <brunoalves.dev@gmail.com>
2022-05-23 20:56:35 -0300

    Tests Adjusts

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nabandeja/main.dart';
+import 'package:nabandeja/widget/login/index.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
